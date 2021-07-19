f7ad1372370f9371242ae567b1786be56e9b08a8
Dan P <73758508+benochi@users.noreply.github.com>
2021-07-19 10:20:16 -0500

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -5,6 +5,8 @@ class functionTestCase(TestCase) #class subclass of TestCase
     """example of unit tests."""
     
     def test_function(self) #test_ "function name"
-        assertEqual folder.function(2, 3) == 5 #unittest has special methods.
-
+        self.assertEqual(folder.function(2, 3) == 5) #unittest has special methods.
+    
+    def test_function2(self):
+        assert folder.function(2, 3) == 5 #wouldn't normally use assert, but for syntax. 
         #use python -m unittest NAME_OF_FILE runs all cases
