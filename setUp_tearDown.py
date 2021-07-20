1b9c9b2a6dc2b5837fb3f7c0343b83da63edf277
Dan P <73758508+benochi@users.noreply.github.com>
2021-07-20 07:23:06 -0500

    Update setUp_tearDown.py

--- setUp_tearDown.py
+++ setUp_tearDown.py
@@ -1,15 +1,21 @@
+from app import app
+from flask import session
+from unittest import TestCase
+
 class FlaskTests(TestCase):
   
   def setUp(self): #must be camel case
     """Stuff to do before every test"""
+    print("Setting up!")
     
   def tearDown(self):
     """Will run after every test method"""
+    print("Tearing down!")
     
   def test_1(self):
-    ...
+    print("test 1 running!")
     
-  #Will run as...
+  #Will run in this order:
   setUp()
   test_1()
   tearDown()
