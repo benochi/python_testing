92e4cdc9781dbc77daa6ab399fc1cf9b7e166c48
Dan P <73758508+benochi@users.noreply.github.com>
2021-07-20 07:00:44 -0500

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -12,3 +12,23 @@ class FunctionTestCase(TestCase) #class subclass of TestCase
     def test_function2(self):
         assert folder.function(2, 3) == 5 #wouldn't normally use assert, but for syntax. 
         #use python -m unittest NAME_OF_FILE runs all cases
+
+        
+    from app import app
+    from unittest import TestCase
+    
+    class ColorViewsTestCase(TestCase):
+        def test_color_form(self):
+            with app.test_client() as client:#client refers to server
+                res = client.get('/')
+                html = res.get_data(as_Text=True)
+                
+                self.assertEqual(res.status_code, 200) #checks response code for 200
+                self.assertIn('<h1>Color Form</h1>', html) #checks response data string for this phrase, html is just variable name
+                
+        def test_redirection(self):
+            with app.test_client() as client:
+                res = client.get('/redirect-me')
+                
+                self.assertEqual(res.status_code, 302)
+                self.assertEqual(res.location, 'http://localhost/') #don't need /loclahost/5000 for example.  
